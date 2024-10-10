6d7d6bddfb506e9b224bdd68752baa46536b4e72
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-10-10 00:17:22 -0300

    Teste: Usuário vê galpões cadastrados, Teste: Usuário vê que não existem galpões cadastrados.

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -8,4 +8,29 @@ describe 'Usuário visita tela incial' do
     # Assert
     expect(page).to have_content('Galpões & Estoque')
   end
+
+  it 'e vê galpões cadastrados' do
+    # Arrange
+    Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
+    Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000)
+    # Act
+    visit('/')
+    # Assert
+    expect(page).not_to have_content('Não existem galpões cadastrados!')
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('Código: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('60000 m2')
+
+    expect(page).to have_content('Maceio')
+    expect(page).to have_content('Código: MCZ')
+    expect(page).to have_content('Cidade: Maceio')
+    expect(page).to have_content('50000 m2')
+  end
+
+  it 'e não tem nenhum galpão cadastrado' do
+    visit('/')
+
+    expect(page).to have_content('Não existem galpões cadastrados!')
+  end
 end
