07294d00ccd1719e4718dc4504d1ca6aa870648f
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-10-12 00:58:28 -0300

    Internacionalização do projeto (i18n) (l10n)

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -12,10 +12,10 @@ class WarehousesController < ApplicationController
     warehouse_params = params.require(:warehouse).permit(:name, :description, :code, :address, :city, :cep, :area)
 
     # 2 -- Criar um novo galpão no banco de dados
-    w = Warehouse.new(warehouse_params)
+    @warehouse = Warehouse.new(warehouse_params)
 
     # 3 -- Salvar o galpão que foi criado
-    if w.save()
+    if @warehouse.save()
       # 4 -- Redirecionar para a tela inicial
       redirect_to root_path, notice: "Galpão cadastrado com sucesso!"
     else
