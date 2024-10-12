d1f35d4e69614590dd63ab2125dfb2f88043a373
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-10-12 14:01:46 -0300

    Teste: Usuário remove um galpão com sucesso, [2] e não afeta outros galpões

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # Defines my roots
 
   root to: "home#index"
-  resources :warehouses, only: [ :show, :new, :create, :edit, :update ]
+  resources :warehouses, only: [ :show, :new, :create, :edit, :update, :destroy ]
 end
---
+++ spec/system/warehouses/user_delete_warehouse_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'Usuário remove um galpão' do
+  it 'com sucesso' do
+    w = Warehouse.create!(name: 'Cuiaba', code: 'CWB', area: 10000, cep: '56000-000', city: 'Cuiabá', description: 'Galpão do centro do país', address: 'Av dos Jacarés, 1000')
+
+    visit root_path
+    click_on 'Cuiaba'
+    click_on 'Remover'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Galpão removido com sucesso'
+    expect(page).not_to have_content 'Cuiaba'
+    expect(page).not_to have_content 'CWB'
+  end
+
+  it 'e não apaga outros galpões' do
+    w = Warehouse.create!(name: 'Cuiaba', code: 'CWB', area: 10000, cep: '56000-000', city: 'Cuiabá', description: 'Galpão do centro do país', address: 'Av dos Jacarés, 1000')
+    wb = Warehouse.create!(name: 'b', code: 'BBB', area: 20000, cep: '16000-000', city: 'bb', description: 'Galpão do bbb', address: 'Av dos Bs, 200')
+
+    visit root_path
+    click_on 'Cuiaba'
+    click_on 'Remover'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Galpão removido com sucesso'
+    expect(page).to have_content 'BBB'
+    expect(page).not_to have_content 'CWB'
+  end
+end
