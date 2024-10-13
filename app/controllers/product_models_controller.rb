537b415ef52c55d80d5ae078d6352de71e28c42e
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-10-13 18:12:09 -0300

    Teste: user register product_models

--- app/controllers/product_models_controller.rb
+++ app/controllers/product_models_controller.rb
@@ -2,4 +2,31 @@ class ProductModelsController < ApplicationController
   def index
     @product_models = ProductModel.all
   end
+
+  def new
+    @product_model = ProductModel.new
+    @suppliers = Supplier.all
+  end
+
+  def create
+    @product_model = ProductModel.new(pega_parametro)
+
+    if @product_model.save()
+      redirect_to @product_model, notice: "Modelo de produto cadastrado com sucesso"
+    else
+      flash[:notice] = "não cadastrado"
+    end
+  end
+
+  def show
+    @product_model = ProductModel.find(params[:id])
+  end
+
+  private
+
+  def pega_parametro
+    product_model_params = params.require(:product_model).permit(
+      :name, :weight, :width, :height, :depth, :sku, :supplier_id
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :warehouses, only: [ :show, :new, :create, :edit, :update, :destroy ]
   resources :suppliers, only: [ :index, :show ]
-  resources :product_models, only: [ :index ]
+  resources :product_models, only: [ :index, :show, :new, :create ]
 end
