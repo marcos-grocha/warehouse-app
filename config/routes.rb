cc9712dad6e689e6c34bca671da2aab68bad053c
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-10-13 13:59:19 -0300

    Teste: user view supplier and details

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   root to: "home#index"
   resources :warehouses, only: [ :show, :new, :create, :edit, :update, :destroy ]
+  resources :suppliers, only: [ :index, :show ]
+  resources :product_models, only: [ :index ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_10_035614) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_13_164448) do
+  create_table "product_models", force: :cascade do |t|
+    t.string "name"
+    t.integer "weight"
+    t.integer "width"
+    t.integer "height"
+    t.integer "depth"
+    t.string "sku"
+    t.integer "supplier_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["supplier_id"], name: "index_product_models_on_supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "corporate_name"
+    t.string "brand_name"
+    t.string "registration_number"
+    t.string "full_address"
+    t.string "city"
+    t.string "state"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "warehouses", force: :cascade do |t|
     t.string "name"
     t.string "code"
@@ -22,4 +47,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_10_035614) do
     t.string "cep"
     t.string "description"
   end
+
+  add_foreign_key "product_models", "suppliers"
 end
