45773d510b0b0f35a8a6b34a15ad9bedf54b30ac
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-10-14 13:48:48 -0300

    Teste: user sign in

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   resources :warehouses, only: [ :show, :new, :create, :edit, :update, :destroy ]
   resources :suppliers, only: [ :index, :show ]
   resources :product_models, only: [ :index, :show, :new, :create ]
+
+  devise_for :users
 end
