25bfd3608b4a0b9637484402a9b845db0dd0036b
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-10-15 20:41:57 -0300

    Teste: add random code to order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    it 'deve ter um código' do
+      u = User.create!(
+        name: 'Leticia', email: 'leticia@unit.com', password: '123456'
+        )
+      w = Warehouse.create!(
+        name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+        address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+        description: 'Galpão destinado para cargas internacionais'
+        )
+      s = Supplier.create!(
+        corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', registration_number: '07317108000151', full_address: 'Av Nacoes Uniddas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br'
+        )
+      o = Order.new(
+        user: u, warehouse: w, supplier: s, estimated_delivery_date: '2022-10-01'
+        )
+
+      result = o.valid?
+
+      expect(result).to be true
+    end
+  end
+
+  describe 'gera um código único e aleatório' do
+    it 'ao criar um novo pedido' do
+      u = User.create!(
+        name: 'Leticia', email: 'leticia@unit.com', password: '123456'
+        )
+      w = Warehouse.create!(
+        name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+        address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+        description: 'Galpão destinado para cargas internacionais'
+        )
+      s = Supplier.create!(
+        corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', registration_number: '07317108000151', full_address: 'Av Nacoes Uniddas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br'
+        )
+      o = Order.new(
+        user: u, warehouse: w, supplier: s, estimated_delivery_date: '2022-10-01'
+        )
+      o2 = Order.new(
+        user: u, warehouse: w, supplier: s, estimated_delivery_date: '2022-11-15'
+        )
+
+      o.save!
+
+      expect(o.code).not_to be_empty
+      expect(o.code.length).to eq 8
+      expect(o.code).not_to eq o2.code
+    end
+  end
 end
--- spec/system/orders/user_register_order_spec.rb
+++ spec/system/orders/user_register_order_spec.rb
@@ -15,7 +15,7 @@ describe 'Usuário cadastra um pedido' do
     Warehouse.create!(
       name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000,
       address: 'Av Atlantica, 50', cep: '80000-000', description: 'Perto do Aeroporto')
-    warehouse = Warehouse.create!(
+    Warehouse.create!(
       name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
       address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
       description: 'Galpão destinado para cargas internacionais')
@@ -26,6 +26,7 @@ describe 'Usuário cadastra um pedido' do
     supplier = Supplier.create!(
       corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', registration_number: '07317108000151', full_address: 'Av Nacoes Uniddas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
 
+    allow(SecureRandom).to receive(:alphanumeric).and_return('ABC12345')
     # Act
     login_as(user)
     visit root_path
@@ -37,6 +38,7 @@ describe 'Usuário cadastra um pedido' do
 
     # Assert
     expect(page).to have_content 'Pedido registrado com sucesso.'
+    expect(page).to have_content 'Pedido ABC12345'
     expect(page).to have_content 'Galpão Destino: GRU | Aeroporto SP'
     expect(page).to have_content 'Fornecedor: Samsung Eletronicos LTDA'
     expect(page).to have_content 'Usuário responsável: Leticia <leticia@unit.com>'
