0b0f0eaa82a35b4a5a5dde1423773506b6a3069f
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-10-15 03:34:02 -0300

    Teste: unit tests for supplier

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,3 +1,4 @@
 class Supplier < ApplicationRecord
+  has_many :product_model
   validates :corporate_name, :brand_name, :registration_number, :full_address, :city, :state, :email, presence: true
 end
--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Supplier, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid' do
+    it 'brand_name is mandatory' do
+      sp = Supplier.new(brand_name: '', corporate_name: 'Samsung Eletronicos LTDA', registration_number: '07317108000151', full_address: 'Av Nacoes Uniddas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+
+      expect(sp.valid?).to eq false
+    end
+    it 'corporate_name is mandatory' do
+      sp = Supplier.new(brand_name: 'Samsung', corporate_name: '', registration_number: '07317108000151', full_address: 'Av Nacoes Uniddas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+
+      expect(sp.valid?).to eq false
+    end
+    it 'registration_number is mandatory' do
+      sp = Supplier.new(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA', registration_number: '', full_address: 'Av Nacoes Uniddas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+
+      expect(sp.valid?).to eq false
+    end
+    it 'full_address is mandatory' do
+      sp = Supplier.new(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA', registration_number: '07317108000151', full_address: '', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+
+      expect(sp.valid?).to eq false
+    end
+    it 'city is mandatory' do
+      sp = Supplier.new(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA', registration_number: '07317108000151', full_address: 'Av Nacoes Uniddas, 1000', city: '', state: 'SP', email: 'sac@samsung.com.br')
+
+      expect(sp.valid?).to eq false
+    end
+    it 'state is mandatory' do
+      sp = Supplier.new(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA', registration_number: '07317108000151', full_address: 'Av Nacoes Uniddas, 1000', city: 'São Paulo', state: '', email: 'sac@samsung.com.br')
+
+      expect(sp.valid?).to eq false
+    end
+    it 'email is mandatory' do
+      sp = Supplier.new(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA', registration_number: '07317108000151', full_address: 'Av Nacoes Uniddas, 1000', city: 'São Paulo', state: 'SP', email: '')
+
+      expect(sp.valid?).to eq false
+    end
+  end
 end
