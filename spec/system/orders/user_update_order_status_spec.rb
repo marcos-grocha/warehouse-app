0a680999cec80c3ba3022d648d20e9218ad57f43
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-10-21 12:11:21 -0300

    Create model StockProduct and update warehouses stock

--- spec/system/orders/user_update_order_status_spec.rb
+++ spec/system/orders/user_update_order_status_spec.rb
@@ -11,7 +11,9 @@ describe 'Usuário informa novo status de pedido' do
     s = Supplier.create!(
       corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', registration_number: '07317108000151', full_address: 'Av Nacoes Uniddas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br'
       )
+    p = ProductModel.create!(supplier: s, name: 'Cadeira Gamer', weight: 5, height: 100, width: 70, depth: 75, sku: 'CAD-GAMER-1234')
     o = Order.create!(user: u, warehouse: w, supplier: s, estimated_delivery_date: 1.day.from_now)
+    OrderItem.create!(order: o, product_model: p, quantity: 5)
 
     login_as u
     visit root_path
@@ -23,6 +25,9 @@ describe 'Usuário informa novo status de pedido' do
     expect(page).to have_content 'Situação do Pedido: Entregue'
     expect(page).not_to have_button 'Marcar como CANCELADO'
     expect(page).not_to have_button 'Marcar como ENTREGUE'
+    expect(StockProduct.count).to eq 5
+    estoque = StockProduct.where(product_model: p, warehouse: w).count
+    expect(estoque).to eq 5
   end
 
   it 'e pedido foi cancelado' do
@@ -35,7 +40,9 @@ describe 'Usuário informa novo status de pedido' do
     s = Supplier.create!(
       corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', registration_number: '07317108000151', full_address: 'Av Nacoes Uniddas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br'
       )
+    p = ProductModel.create!(supplier: s, name: 'Cadeira Gamer', weight: 5, height: 100, width: 70, depth: 75, sku: 'CAD-GAMER-1234')
     o = Order.create!(user: u, warehouse: w, supplier: s, estimated_delivery_date: 1.day.from_now)
+    OrderItem.create!(order: o, product_model: p, quantity: 5)
 
     login_as u
     visit root_path
@@ -46,5 +53,6 @@ describe 'Usuário informa novo status de pedido' do
     expect(current_path).to eq order_path(o.id)
     expect(page).to have_content 'Situação do Pedido: Cancelado'
     expect(page).not_to have_button 'Marcar como CANCELADO'
+    expect(StockProduct.count).to eq 0
   end
 end
