d4de1f6e7f30c1bb0a3544e053adef458f07fd00
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-10-28 02:15:26 -0300

    TDD: user view stock from warehouse

---
+++ spec/system/product_stock/user_view_stock_from_warehouse_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'Usuário vê o estoque' do
+  it 'na tela de um galpão' do
+    u = User.create!(name: 'Marcos', email: 'marcos@unit.com', password: '123456')
+    w = Warehouse.create!(
+      name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000,
+      address: 'Av Atlantica, 50', cep: '80000-000', description: 'Perto do Aeroporto')
+    s = Supplier.create!(
+      corporate_name: 'Spark Industries Brasil LTDA', brand_name: 'Spark', registration_number: '16074559000104',
+      full_address: 'Torre da Indústria, 1', city: 'Teresina', state: 'PI', email: 'vendas@spark.com.br')
+    o = Order.create!(
+      user: u, warehouse: w, supplier: s, estimated_delivery_date: 1.week.from_now, status: :delivered)
+    p_cadeira = ProductModel.create!(name: 'Cadeira Gamer', weight: 5, height: 70, width: 75, depth: 80, sku: 'CGMER-XPTO-888', supplier: s)
+    p_soundbar = ProductModel.create!(name: 'Soundbar 7.1 Surround', weight: 3000, height: 70, width: 80, depth: 20, sku: 'SOU71-SAMS-888', supplier: s)
+    p_notebook = ProductModel.create!(name: 'Notebook', weight: 2000, height: 90, width: 40, depth: 20, sku: 'NOTI5-SAMS-777', supplier: s)
+
+    3.times { StockProduct.create!(order: o, warehouse: w, product_model: p_cadeira) }
+    2.times { StockProduct.create!(order: o, warehouse: w, product_model: p_notebook) }
+
+    login_as u
+    visit root_path
+    click_on 'Maceio'
+
+    expect(page).to have_content 'Itens em Estoque'
+    expect(page).to have_content '3 x CGMER-XPTO-888'
+    expect(page).to have_content '2 x NOTI5-SAMS-777'
+    expect(page).not_to have_content p_soundbar.sku
+  end
+end
